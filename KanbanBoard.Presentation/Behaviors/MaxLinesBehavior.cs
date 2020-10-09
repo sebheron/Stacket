44ad6cc01180cf1d99c747b6fc48ca2c0783baa4
Aidan Horton <aidan.horton@virginmedia.com>
2020-10-09 14:26:14 +0100

    Replace dodgy ControlzEx with official XAML behaviors

--- KanbanBoard.Presentation/Behaviors/MaxLinesBehavior.cs
+++ KanbanBoard.Presentation/Behaviors/MaxLinesBehavior.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 
 namespace KanbanBoard.Presentation.Behaviors
 {
