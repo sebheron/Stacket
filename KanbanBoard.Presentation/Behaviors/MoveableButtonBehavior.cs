04e878b5c064332e065f05ed5712e453ac990315
Seb Heron <sebheron21@hotmail.co.uk>
2020-10-26 18:12:19 +0000

    Applied aidans code review

--- KanbanBoard.Presentation/Behaviors/MoveableButtonBehavior.cs
+++ KanbanBoard.Presentation/Behaviors/MoveableButtonBehavior.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
-using Microsoft.Xaml.Behaviors;
 
 namespace KanbanBoard.Presentation.Behaviors
 {
     public class MoveableButtonBehavior : Behavior<UIElement>
     {
-        private const int dragThreshold = 1;
-        private const int snapThreshold = 15;
+        private const int DragThreshold = 1;
+        private const int SnapThreshold = 15;
 
         private bool dragging;
         private Point prevPos;
@@ -19,77 +19,75 @@ namespace KanbanBoard.Presentation.Behaviors
         private double halfButtonWidth;
 
         private double xPos;
-        private TranslateTransform transform = new TranslateTransform();
+        private readonly TranslateTransform transform = new TranslateTransform();
         private Window parent;
 
         protected override void OnAttached()
         {
-            if (this.AssociatedObject != null)
-            {
-                parent = Application.Current.MainWindow;
-                AssociatedObject.RenderTransform = transform;
-                xPos = transform.X = Properties.Settings.Default.TogglePosition;
-                halfScreenWidth = SystemParameters.MaximizedPrimaryScreenWidth / 2;
-                halfButtonWidth = ((ToggleButton)AssociatedObject).Width / 2;
+            if (this.AssociatedObject == null) return;
+            this.parent = Application.Current.MainWindow;
+            this.AssociatedObject.RenderTransform = this.transform;
+            this.xPos = this.transform.X = Properties.Settings.Default.TogglePosition;
+            this.halfScreenWidth = SystemParameters.MaximizedPrimaryScreenWidth / 2;
+            this.halfButtonWidth = ((ToggleButton)this.AssociatedObject).Width / 2;
 
-                AssociatedObject.PreviewMouseLeftButtonDown += AssociatedObject_PreviewMouseLeftButtonDown;
-                AssociatedObject.PreviewMouseLeftButtonUp += AssociatedObject_PreviewMouseLeftButtonUp;
-                AssociatedObject.PreviewMouseMove += AssociatedObject_PreviewMouseMove;
-            }
+            this.AssociatedObject.PreviewMouseLeftButtonDown += this.AssociatedObject_PreviewMouseLeftButtonDown;
+            this.AssociatedObject.PreviewMouseLeftButtonUp += this.AssociatedObject_PreviewMouseLeftButtonUp;
+            this.AssociatedObject.PreviewMouseMove += this.AssociatedObject_PreviewMouseMove;
         }
 
         protected override void OnDetaching()
         {
-            if (this.AssociatedObject != null)
-            {
-                AssociatedObject.PreviewMouseLeftButtonDown -= AssociatedObject_PreviewMouseLeftButtonDown;
-                AssociatedObject.PreviewMouseLeftButtonUp -= AssociatedObject_PreviewMouseLeftButtonUp;
-                AssociatedObject.PreviewMouseMove -= AssociatedObject_PreviewMouseMove;
-            }
+            if (this.AssociatedObject == null) return;
+            this.AssociatedObject.PreviewMouseLeftButtonDown -= this.AssociatedObject_PreviewMouseLeftButtonDown;
+            this.AssociatedObject.PreviewMouseLeftButtonUp -= this.AssociatedObject_PreviewMouseLeftButtonUp;
+            this.AssociatedObject.PreviewMouseMove -= this.AssociatedObject_PreviewMouseMove;
         }
 
         private void AssociatedObject_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            var delta = e.GetPosition(parent).X - prevPos.X;
-            if (!Properties.Settings.Default.LockToggle &&
-                e.LeftButton == MouseButtonState.Pressed &&
-                (Math.Abs(delta) > dragThreshold || dragging))
+            var delta = e.GetPosition(this.parent).X - this.prevPos.X;
+
+            if (!Properties.Settings.Default.LockToggle
+                && e.LeftButton == MouseButtonState.Pressed
+                && (Math.Abs(delta) > DragThreshold || this.dragging))
             {
-                dragging = true;
-                xPos += delta;
-                if (Math.Abs(xPos) < snapThreshold)
+                this.dragging = true;
+                this.xPos += delta;
+
+                if (Math.Abs(xPos) < SnapThreshold)
                 {
-                    transform.X = 0;
+                    this.transform.X = 0;
                 }
-                else if (xPos > halfScreenWidth - halfButtonWidth)
+                else if (this.xPos > this.halfScreenWidth - this.halfButtonWidth)
                 {
-                    transform.X = halfScreenWidth - halfButtonWidth;
+                    this.transform.X = this.halfScreenWidth - this.halfButtonWidth;
                 }
-                else if (xPos < -halfScreenWidth + halfButtonWidth)
+                else if (this.xPos < -this.halfScreenWidth + this.halfButtonWidth)
                 {
-                    transform.X = -halfScreenWidth + halfButtonWidth;
+                    this.transform.X = -this.halfScreenWidth + this.halfButtonWidth;
                 }
                 else
                 {
-                    transform.X = xPos;
+                    this.transform.X = this.xPos;
                 }
             }
-            prevPos = e.GetPosition(parent);
+            this.prevPos = e.GetPosition(this.parent);
         }
 
         private void AssociatedObject_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            dragging = false;
-            prevPos = e.GetPosition(parent);
+            this.dragging = false;
+            this.prevPos = e.GetPosition(this.parent);
         }
 
         private void AssociatedObject_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (dragging)
+            if (this.dragging)
             {
-                dragging = false;
+                this.dragging = false;
                 e.Handled = true;
-                Properties.Settings.Default.TogglePosition = xPos = transform.X;
+                Properties.Settings.Default.TogglePosition = this.xPos = this.transform.X;
                 Properties.Settings.Default.Save();
                 ((ToggleButton)sender).ReleaseMouseCapture();
             }
