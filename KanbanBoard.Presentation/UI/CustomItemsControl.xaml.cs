f9486b9f61a909a1d273c98d4baca1a854ed599b
Seb Heron <sebheron21@hotmail.co.uk>
2021-01-21 14:24:49 +0000

    Cursor now shows on dragging

---
+++ KanbanBoard.Presentation/UI/CustomItemsControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanbanBoard.Presentation.UI
+{
+    /// <summary>
+    /// Interaction logic for CustomUserControl.xaml
+    /// </summary>
+    public partial class CustomItemsControl : ItemsControl
+    {
+        public CustomItemsControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            if (e.Effects == DragDropEffects.Move)
+            {
+                Mouse.SetCursor((Cursor)this.Resources["GrabCursor"]);
+            }
+            e.Handled = true;
+        }
+    }
+}
